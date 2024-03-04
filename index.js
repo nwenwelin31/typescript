ba9cdd21b083b6e1963c67227813a346edbb3df2
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-03-04 13:25:51 +0630

    type basics

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+let age = 20;
+let person = 'Nwe Nwe';
+let single = true;
+let fruits = ['banna', 'orange', 'apple'];
+fruits.push('strawberry');
+let ages = [20, 30, 40, 50];
+ages.push(10);
+console.log(fruits);
+console.log(ages);
+let myself = {
+    name: 'Nwe Nwe',
+    age: 20,
+    married: false,
+};
+myself.age = 'Lin';
+console.log(myself);
---
+++ index.ts
@@ -0,0 +1,17 @@
+let age:number = 20;
+let person:string = 'Nwe Nwe';
+let single:boolean = true;
+
+let fruits = ['banna','orange','apple'];
+fruits.push('strawberry');
+let ages = [20,30,40,50];
+ages.push(10);
+console.log(fruits);
+console.log(ages);
+
+let myself = {
+    name: 'Nwe Nwe',
+    age: 20,
+    married: false,
+}
+console.log(myself);
